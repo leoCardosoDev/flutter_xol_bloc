8b3f802cd0b90d8433246bb78e8774797a3efd66
Leonardo Silva <leonardo.silva@simpaul.com.br>
2021-01-11 14:06:46 -0300

    feat: add bloc to drwer

--- lib/common/drawer/widgets/icon_section.dart
+++ lib/common/drawer/widgets/icon_section.dart
@@ -1,38 +1,60 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:xlo/blocs/drawer_bloc.dart';
 
 import 'icon_tile.dart';
 
 class IconSection extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        IconTile(
-            label: 'Anúncios',
-            iconData: Icons.list,
-            onTap: () {},
-            highlighted: true),
-        IconTile(
-            label: 'Inserir Anúncios',
-            iconData: Icons.edit,
-            onTap: () {},
-            highlighted: false),
-        IconTile(
-            label: 'Chat',
-            iconData: Icons.chat,
-            onTap: () {},
-            highlighted: false),
-        IconTile(
-            label: 'Favoritos',
-            iconData: Icons.favorite,
-            onTap: () {},
-            highlighted: false),
-        IconTile(
-            label: 'Minha Conta',
-            iconData: Icons.person,
-            onTap: () {},
-            highlighted: false),
-      ],
-    );
+    final DrawerBloc _drawerBloc = Provider.of<DrawerBloc>(context);
+    void _setPage(int page) {
+      Navigator.of(context).pop();
+      _drawerBloc.setPage(page);
+    }
+
+    return StreamBuilder<Object>(
+        stream: _drawerBloc.outPage,
+        builder: (context, snapshot) {
+          return Column(
+            children: [
+              IconTile(
+                  label: 'Anúncios',
+                  iconData: Icons.list,
+                  onTap: () {
+                    _setPage(0);
+                  },
+                  highlighted: snapshot.data == 0),
+              IconTile(
+                  label: 'Inserir Anúncios',
+                  iconData: Icons.edit,
+                  onTap: () {
+                    _setPage(1);
+                  },
+                  highlighted: snapshot.data == 1),
+              IconTile(
+                  label: 'Chat',
+                  iconData: Icons.chat,
+                  onTap: () {
+                    _setPage(2);
+                  },
+                  highlighted: snapshot.data == 2),
+              IconTile(
+                  label: 'Favoritos',
+                  iconData: Icons.favorite,
+                  onTap: () {
+                    _setPage(3);
+                  },
+                  highlighted: snapshot.data == 3),
+              IconTile(
+                  label: 'Minha Conta',
+                  iconData: Icons.person,
+                  onTap: () {
+                    _setPage(4);
+                  },
+                  highlighted: snapshot.data == 4),
+            ],
+          );
+        });
   }
 }
