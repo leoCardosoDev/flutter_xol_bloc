647291033983d7eba2156108ba9c78a9e4c9bca6
Leonardo Silva <leonardo.silva@simpaul.com.br>
2021-01-11 09:43:44 -0300

    feat: add base and home screen

---
+++ lib/screens/base/base_screen.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:xlo/screens/home/home_screen.dart';
+
+class BaseScreen extends StatefulWidget {
+  @override
+  _BaseScreenState createState() => _BaseScreenState();
+}
+
+class _BaseScreenState extends State<BaseScreen> {
+  final PageController _pageController = PageController();
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: PageView(
+        controller: _pageController,
+        physics: const NeverScrollableScrollPhysics(),
+        children: [
+          HomeScreen(),
+          Container(color: Colors.amber),
+          Container(color: Colors.green),
+          Container(color: Colors.black),
+          Container(color: Colors.blue),
+        ],
+      ),
+    );
+  }
+}
