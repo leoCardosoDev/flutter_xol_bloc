ab6e5dfdfd5599061cbb80677bf4339a30dedc8e
Leonardo Silva <leonardo.silva@simpaul.com.br>
2021-01-11 15:47:30 -0300

    feat: add search dialog stack

--- lib/screens/home/home_screen.dart
+++ lib/screens/home/home_screen.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:xlo/common/drawer/custom_drawer.dart';
 
+import 'widgets/search_dialog.dart';
+
 class HomeScreen extends StatefulWidget {
   @override
   _HomeScreenState createState() => _HomeScreenState();
@@ -9,9 +11,23 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
+    _openSearch(String currentSearch) async {
+      await showDialog(
+          context: context,
+          builder: (context) => SearchDialog(currentSearch: currentSearch));
+    }
+
     return Scaffold(
       appBar: AppBar(
         title: Text('XLO'),
+        actions: [
+          IconButton(
+            icon: Icon(Icons.search),
+            onPressed: () {
+              _openSearch("");
+            },
+          ),
+        ],
       ),
       drawer: CustomDrawer(),
     );
